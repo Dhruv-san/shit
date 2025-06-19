10f0b3b75753e99b477443168374e1eac22699bf
Dhruv-san <dhruvvermasr2421@gmail.com>
2025-06-19 13:50:30 +0000

    Wrap app in NhostClientProvider to fix Nhost auth client error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/context/AuthContext'
+import { NhostClientProvider } from '@/components/providers/NhostClientProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black text-white`}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <NhostClientProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </NhostClientProvider>
       </body>
     </html>
   )
